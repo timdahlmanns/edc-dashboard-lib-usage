02ad2cb1d32e999dcf3fda1c03f479621b3660e4
Tim Dahlmanns <tim.dahlmanns@isst.fraunhofer.de>
2026-05-27 09:17:32 +0200

    feat: lib usage
    fix: http client

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('@eclipse-edc/dashboard-core/home').then(m => m.HomeViewComponent),
+  },
+  {
+    path: 'assets',
+    loadComponent: () => import('@eclipse-edc/dashboard-core/assets').then(m => m.AssetViewComponent),
+  },
+  {
+    path: 'policies',
+    loadComponent: () => import('@eclipse-edc/dashboard-core/policies').then(m => m.PolicyViewComponent),
+  },
+  {
+    path: 'contract-definitions',
+    loadComponent: () =>
+      import('@eclipse-edc/dashboard-core/contract-definitions').then(m => m.ContractDefinitionsViewComponent),
+  },
+  {
+    path: 'contracts',
+    loadComponent: () => import('@eclipse-edc/dashboard-core/transfer').then(m => m.ContractViewComponent),
+  },
+
+  {
+    path: 'catalog',
+    loadComponent: () => import('@eclipse-edc/dashboard-core/catalog').then(m => m.CatalogViewComponent),
+  },
+  {
+    path: 'transfer-history',
+    loadComponent: () => import('@eclipse-edc/dashboard-core/transfer').then(m => m.TransferHistoryViewComponent),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, signal, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import { DashboardAppComponent, EdcConfig, AppConfig } from '@eclipse-edc/dashboard-core';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [DashboardAppComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('lib-test');
+
+  private readonly http = inject(HttpClient);
+
+  protected readonly themes = [
+    'light',
+    'dark',
+    'dim',
+    'aqua',
+    'nord',
+    'synthwave',
+    'forest',
+    'dracula',
+    'night',
+    'coffee',
+    'emerald',
+  ];
+  edcConfigs?: Promise<EdcConfig[]>;
+  appConfig?: Promise<AppConfig>;
+
+  ngOnInit() {
+    this.edcConfigs = firstValueFrom(this.http.get<EdcConfig[]>('config/edc-connector-config.json'));
+    this.appConfig = firstValueFrom(this.http.get<AppConfig>('config/app-config.json'));
+  }
 }
